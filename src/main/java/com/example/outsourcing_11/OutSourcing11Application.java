4c865aca9b10a1276dd09f2a8c0d76c48f0bb37d
escomputer <nujest307@naver.com>
2025-04-22 21:24:33 +0900

    create project

---
+++ src/main/java/com/example/outsourcing_11/OutSourcing11Application.java
@@ -0,0 +1,13 @@
+package com.example.outsourcing_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutSourcing11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutSourcing11Application.class, args);
+    }
+
+}
