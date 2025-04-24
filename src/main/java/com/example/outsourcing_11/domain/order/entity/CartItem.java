ceaffe01ecd43b232dad454e7f475391b82681b7
남유리 <skadbfl11225@gmail.com>
2025-04-24 12:00:40 +0900

    주문관련 기능 업데이트

---
+++ src/main/java/com/example/outsourcing_11/domain/order/entity/CartItem.java
@@ -0,0 +1,32 @@
+package com.example.outsourcing_11.domain.order.entity;
+
+import com.example.outsourcing_11.domain.menu.entity.Menu;
+import com.example.outsourcing_11.domain.store.entity.Store;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+//
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+//
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    // 생성자, getter, setter
+}
