f8f120a8f251942c6bbf1a4b6e50286b3f8fc06d
escomputer <nujest307@naver.com>
2025-04-24 11:22:43 +0900

    Fix : dto 분리

--- src/main/java/com/example/outsourcing_11/domain/store/dto/StoreListDto.java
+++ src/main/java/com/example/outsourcing_11/domain/store/dto/StoreListDto.java
@@ -1,21 +1,27 @@
 package com.example.outsourcing_11.domain.store.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import com.example.outsourcing_11.domain.store.entity.Store;
 import com.example.outsourcing_11.domain.store.entity.StoreCategory;
 import com.example.outsourcing_11.domain.store.entity.StoreStatus;
 
 /**
- * 가게 다건 조회용
+ * 리스트 다건 조회용
  */
 @Getter
-@AllArgsConstructor
 public class StoreListDto {
 	private Long id;
-	private String name;
-	private int minimumOrderPrice;
-	private StoreStatus status;
-	private StoreCategory category;
+	private final String name;
+	private final int minimumOrderPrice;
+	private final StoreStatus status;
+	private final StoreCategory category;
 
+	public StoreListDto(Store store) {
+		this.id = store.getId();
+		this.name = store.getName();
+		this.minimumOrderPrice = store.getMinimumOrderPrice();
+		this.status = store.getStatus();
+		this.category = store.getCategory();
+	}
 }
