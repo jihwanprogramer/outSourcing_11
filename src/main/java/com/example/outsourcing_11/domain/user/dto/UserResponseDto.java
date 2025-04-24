dbe4abcd3375b3d3e86287add604813b15ec0425
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-04-24 19:11:54 +0900

    fix: 로그인된 사용자조회 변경
    사장님일경우 등록된 가게같이 조회

--- src/main/java/com/example/outsourcing_11/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/example/outsourcing_11/domain/user/dto/UserResponseDto.java
@@ -1,9 +1,13 @@
 package com.example.outsourcing_11.domain.user.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import com.example.outsourcing_11.domain.store.dto.StoreDto;
 
 @Getter
 @NoArgsConstructor
@@ -15,5 +19,13 @@ public class UserResponseDto {
 	private String phone;
 	private String address;
 	private String role;
-	// private final Store store; 사장님일 경우 가지고있는 가게도 같이 조회예정
+	private List<StoreDto> storeList; //사장님일 경우 가지고있는 가게도 같이 조회
+
+	public UserResponseDto(String userName, String email, String phone, String address, String role) {
+		this.userName = userName;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+		this.role = role;
+	}
 }
