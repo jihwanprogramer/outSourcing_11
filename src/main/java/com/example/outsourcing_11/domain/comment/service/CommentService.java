24a588239ae6355fcac4f270fad6393559fc5a5d
Fragon02 <chj20202@naver.com>
2025-04-24 19:13:44 +0900

    Test: bugTest

--- src/main/java/com/example/outsourcing_11/domain/comment/service/CommentService.java
+++ src/main/java/com/example/outsourcing_11/domain/comment/service/CommentService.java
@@ -2,18 +2,15 @@ package com.example.outsourcing_11.domain.comment.service;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import com.example.outsourcing_11.domain.comment.dto.RequestCommentDto;
 import com.example.outsourcing_11.domain.comment.dto.ResponseCommentDto;
 
 public interface CommentService {
-	ResponseCommentDto createComment(@PathVariable Long storeId, @PathVariable Long orderId,
-		@RequestBody RequestCommentDto dto);
+	ResponseCommentDto createComment(Long orderId, Long userId, RequestCommentDto dto);
 
-	List<ResponseCommentDto> findCommentsByRatingRange(int min, int max);
+	List<ResponseCommentDto> findCommentsByRatingRange(Long orderId, Long userId, int min, int max);
 
-	ResponseCommentDto updateComment(Long commentid, @RequestBody RequestCommentDto dto);
+	ResponseCommentDto updateComment(Long orderId, Long userId, Long commentId, RequestCommentDto dto);
 
-	void deleteComment(Long id);
+	void deleteComment(Long orderId, Long userId, Long commentId);
 }
--- src/main/java/com/example/outsourcing_11/domain/comment/service/CommentServiceImple.java
+++ src/main/java/com/example/outsourcing_11/domain/comment/service/CommentServiceImple.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import com.example.outsourcing_11.common.Status;
+import com.example.outsourcing_11.common.exception.CustomException;
 import com.example.outsourcing_11.domain.comment.dto.RequestCommentDto;
 import com.example.outsourcing_11.domain.comment.dto.ResponseCommentDto;
 import com.example.outsourcing_11.domain.comment.entity.Comment;
 import com.example.outsourcing_11.domain.comment.repository.CommentRepository;
 import com.example.outsourcing_11.domain.order.entity.Order;
+import com.example.outsourcing_11.domain.order.entity.OrderStatus;
 import com.example.outsourcing_11.domain.order.repository.OrderRepository;
 
 @Service
@@ -21,19 +24,14 @@ public class CommentServiceImple implements CommentService {
 	private static OrderRepository orderRepository;
 
 	@Override
-	public ResponseCommentDto createComment(Long storeId, Long orderId, RequestCommentDto dto) {
-		//주문상태를 확인하고 Complete 아닐경우 예외 발생하기.
-
-		Order order = orderRepository.getReferenceById(orderId);
-		// if(!order..equals(storeId)){
-		// 	return  new RuntimeException("주문한 가계가 아닙니다.", HttpStatus.BAD_REQUEST);
-		// }
-		// if(!order.getUser().getId().equals(userId)){
-		// 	return  new RuntimeException("권한이 없습니다.",HttpStatus.BAD_REQUEST);
-		// }
-		// if (!order.getStatus() == "Complete") {
-		// 	return new RuntimeException("배달이 완료되지 않았습니다.", HttpStatus.BAD_REQUEST);
-		// }
+	public ResponseCommentDto createComment(Long orderId, Long userId, RequestCommentDto dto) {
+		Order order = orderRepository.findById(orderId).orElseThrow();
+		if (!order.getUser().equals(userId)) {
+			throw new CustomException("권한이 없습니다.", HttpStatus.BAD_REQUEST);
+		}
+		if (!order.getStatus().equals(OrderStatus.COMPLETED)) {
+			throw new CustomException("배달완료가되지 않았습니다.", HttpStatus.BAD_REQUEST);
+		}
 
 		Comment savecomment = new Comment(dto);
 		commentRepository.save(savecomment);
@@ -41,53 +39,47 @@ public class CommentServiceImple implements CommentService {
 	}
 
 	@Override
-	public List<ResponseCommentDto> findCommentsByRatingRange(int min, int max) {
+	public List<ResponseCommentDto> findCommentsByRatingRange(Long orderId, Long userId, int min, int max) {
 
-		// Order order = findByOrder(orderId);
-		// if(!order.getStore().store.getId().equals(storeId)){
-		// 	return  new RuntimeException("주문한 가계가 아닙니다.", HttpStatus.BAD_REQUEST);
-		// }
-		// if(!order.getUser().getId().equals(userId)){
-		// 	return  new RuntimeException("조회 권한이 없습니다.", HttpStatus.BAD_REQUEST);
-		// }
-		// if (!order.) {
-		// 	return new RuntimeException("배달이 완료되지 않았습니다.", HttpStatus.BAD_REQUEST);
-		// }
+		Order order = orderRepository.findById(orderId).orElseThrow();
+		if (!order.getUser().equals(userId)) {
+			throw new CustomException("권한이 없습니다.", HttpStatus.BAD_REQUEST);
+		}
+		if (!order.getStatus().equals(OrderStatus.COMPLETED)) {
+			throw new CustomException("배달완료가되지 않았습니다.", HttpStatus.BAD_REQUEST);
+		}
+
 		List<Comment> comments = commentRepository.findByRatingBetweenAndDeletedAtIsNull(min, max);
 		return comments.stream().map(ResponseCommentDto::new).toList();
 	}
 
 	@Override
-	public ResponseCommentDto updateComment(Long commentId, RequestCommentDto dto) {
+	public ResponseCommentDto updateComment(Long orderId, Long userId, Long commentId, RequestCommentDto dto) {
 
-		// Order order = findByOrder(orderId);
-		// if(!order.getStore().store.getId().equals(storeId)){
-		// 	return  new RuntimeException("주문한 가계가 아닙니다.", HttpStatus.BAD_REQUEST);
-		// }
-		// if(!order.getUser().getId().equals(userId)){
-		// 	return  new RuntimeException("수정 권한이 없습니다.",HttpStatus.BAD_REQUEST);
-		// }
-		// if (!order.getStatus() == "Complete") {
-		// 	return new RuntimeException("배달이 완료되지 않았습니다.", HttpStatus.BAD_REQUEST);
-		// }
-		// Comment findcomment = commentRepository.findByOrThrowElse(commentId);
-		// findcomment = new Comment(dto);
-		return null;
+		Order order = orderRepository.findById(orderId).orElseThrow();
+		if (!order.getUser().equals(userId)) {
+			throw new CustomException("권한이 없습니다.", HttpStatus.BAD_REQUEST);
+		}
+		if (!order.getStatus().equals(OrderStatus.COMPLETED)) {
+			throw new CustomException("배달완료가되지 않았습니다.", HttpStatus.BAD_REQUEST);
+		}
+
+		Comment findcomment = commentRepository.findByOrThrowElse(commentId);
+		findcomment = new Comment(dto);
+		return new ResponseCommentDto(findcomment);
 	}
 
 	@Override
-	public void deleteComment(Long commentId) {
+	public void deleteComment(Long orderId, Long userId, Long commentId) {
 
-		// Order order = findByOrder(orderId);
-		// if(!order.getStore().store.getId().equals(storeId)){
-		// 	return  new RuntimeException("주문한 가계가 아닙니다.", HttpStatus.BAD_REQUEST);
-		// }
-		// if(!order.getUser().getId().equals(userId)){
-		// 	return  new RuntimeException("수정 권한이 없습니다.",HttpStatus.BAD_REQUEST);
-		// }
-		// if (!order.getStatus().equals("Complete")) {
-		// 	return new RuntimeException("배달이 완료되지 않았습니다.", HttpStatus.BAD_REQUEST);
-		// }
+		Order order = orderRepository.findById(orderId).orElseThrow();
+		if (!order.getUser().equals(userId)) {
+			throw new CustomException("권한이 없습니다.", HttpStatus.BAD_REQUEST);
+		}
+		if (!order.getStatus().equals(OrderStatus.COMPLETED)) {
+			throw new CustomException("배달완료가되지 않았습니다.", HttpStatus.BAD_REQUEST);
+		}
+
 		//softDelete 진행.
 		Comment findcomment = commentRepository.findByOrThrowElse(commentId);
 		Status status = Status.fromValue(true);
