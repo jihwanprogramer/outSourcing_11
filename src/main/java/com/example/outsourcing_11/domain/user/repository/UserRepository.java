becfe0bbc045797066040d3a416b33c834f6c347
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-04-24 17:28:06 +0900

    사소한 수정

--- src/main/java/com/example/outsourcing_11/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/outsourcing_11/domain/user/repository/UserRepository.java
@@ -14,13 +14,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	// Id로 조회
 	Optional<User> findById(Long id);//
 
-	//이메일통해 존재하는지만 판단
+	// 이메일통해 존재하는지만 판단
 	boolean existsByEmail(String email);
 
-	// id로 조회
+	// Id통해 존재하는지만 판단
+	boolean existsById(Long id);
+
+	// email로 조회
 	default User findByEmailOrElseThrow(String email) {
 		return findByEmail(email).orElseThrow(() ->
-			new UserNotFoundException("존재하지 않는 정보입니다.")); // 조회 실패시(해당 id 없을 때) 404, "존재하지 않는 정보입니다."
+			new UserNotFoundException("존재하지 않는 정보입니다.")); // 조회 실패시(해당 email 없을 때) 404, "존재하지 않는 정보입니다."
 	}
 
 	// id로 조회
