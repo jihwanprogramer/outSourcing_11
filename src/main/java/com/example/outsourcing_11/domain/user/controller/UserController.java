e42e3f37c39f65266d748b9ec9f93e539e1110e1
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-04-24 11:38:13 +0900

    feat: 유저정보조회 로그인한 유저정보조회 기능

--- src/main/java/com/example/outsourcing_11/domain/user/controller/UserController.java
+++ src/main/java/com/example/outsourcing_11/domain/user/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.example.outsourcing_11.domain.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.outsourcing_11.domain.user.dto.UserResponseDto;
+import com.example.outsourcing_11.domain.user.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+	private final UserService userService;
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserResponseDto> findUserById(@PathVariable Long userId) {
+		UserResponseDto responseDto = userService.findUserById(userId);
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
+	}
+
+	@GetMapping("/checkLogin")
+	public ResponseEntity<UserResponseDto> findLoginUserById(HttpServletRequest request) {
+		UserResponseDto responseDto = userService.findLoginUserById(request);
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
+	}
 }
--- src/main/java/com/example/outsourcing_11/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/example/outsourcing_11/domain/user/dto/UserResponseDto.java
@@ -1,18 +1,15 @@
-package com.example.outsourcing_11.domain.auth.dto;
-
-import java.time.LocalDateTime;
+package com.example.outsourcing_11.domain.user.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class SignUpResponseDto {
-
+public class UserResponseDto {
 	private final String userName;
 	private final String email;
 	private final String phone;
 	private final String address;
 	private final String role;
-	private final LocalDateTime createAt;
+	// private final Store store; 사장님일 경우 가지고있는 가게도 같이 조회예정
 }
