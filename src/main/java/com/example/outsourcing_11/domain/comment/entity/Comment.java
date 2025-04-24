0cf0bcdf204446a14e1cd6e9cd03e794ad3079c1
Fragon02 <chj20202@naver.com>
2025-04-24 14:49:02 +0900

    fix: @GeneratedValue 설정수정

--- src/main/java/com/example/outsourcing_11/domain/comment/entity/Comment.java
+++ src/main/java/com/example/outsourcing_11/domain/comment/entity/Comment.java
@@ -17,6 +17,8 @@ import lombok.Getter;
 
 import com.example.outsourcing_11.common.Base;
 import com.example.outsourcing_11.domain.comment.dto.RequestCommentDto;
+import com.example.outsourcing_11.domain.order.entity.Order;
+import com.example.outsourcing_11.domain.store.entity.Store;
 import com.example.outsourcing_11.domain.user.entity.User;
 
 @Getter
@@ -24,7 +26,7 @@ import com.example.outsourcing_11.domain.user.entity.User;
 @Table(name = "comments")
 public class Comment extends Base {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
@@ -33,9 +35,11 @@ public class Comment extends Base {
 	@OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Comment> childrenComment = new ArrayList<>();
 
-	// @ManyToOne(fetch = FetchType.LAZY)
-	// private Order order;
-	//
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Order order;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Store store;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	private User user;
 
@@ -58,8 +62,23 @@ public class Comment extends Base {
 		this.rating = dto.getRating();
 	}
 
+	public Comment(String content, String imageUrl, int rating) {
+
+		this.content = content;
+		this.imageUrl = imageUrl;
+		this.rating = rating;
+	}
+
 	public void updateDeleteStatus(boolean isDeleted) {
 		this.isDeleted = isDeleted;
+	}
+
+	public void setUser(User setUser) {
+		this.user = setUser;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
 }
