26146d50d632c7d691e66436b62dc7d4bea2fe43
escomputer <nujest307@naver.com>
2025-04-25 13:15:41 +0900

    fix : jpql 수정

--- src/main/java/com/example/outsourcing_11/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/outsourcing_11/domain/order/repository/OrderRepository.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.outsourcing_11.domain.order.entity.Order;
 import com.example.outsourcing_11.domain.store.entity.Store;
@@ -14,6 +16,11 @@ import com.example.outsourcing_11.domain.store.entity.Store;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	List<Order> findByUserId(Long userId);
 
-	BigDecimal sumTotalPriceByStoreAndCreatedAtBetween(Store store, LocalDateTime start, LocalDateTime now);
+	@Query("SELECT SUM(o.totalPrice) FROM Order o WHERE o.store = :store AND o.orderDate BETWEEN :start AND :now")
+	BigDecimal sumTotalPriceByStoreAndCreatedAtBetween(
+		@Param("store") Store store,
+		@Param("start") LocalDateTime start,
+		@Param("now") LocalDateTime now
+	);
 }
 //
