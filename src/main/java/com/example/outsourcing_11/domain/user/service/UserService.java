bbf96cc815debf7cd410c51cd7024030eacd2ea5
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-04-25 18:05:58 +0900

    feat: 회원정보수정 완료

--- src/main/java/com/example/outsourcing_11/domain/user/service/UserService.java
+++ src/main/java/com/example/outsourcing_11/domain/user/service/UserService.java
@@ -13,12 +13,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.example.outsourcing_11.common.UserRole;
 import com.example.outsourcing_11.common.exception.user.InvalidLoginException;
+import com.example.outsourcing_11.common.exception.user.InvalidUserInputException;
 import com.example.outsourcing_11.common.exception.user.UnauthorizedException;
 import com.example.outsourcing_11.common.exception.user.UserNotFoundException;
 import com.example.outsourcing_11.config.PasswordEncoder;
 import com.example.outsourcing_11.config.security.CustomUserDetails;
-import com.example.outsourcing_11.domain.user.dto.DeleteUserResponseDto;
+import com.example.outsourcing_11.domain.user.dto.DeleteAndUpdateUserResponseDto;
 import com.example.outsourcing_11.domain.user.dto.PasswordRequestDto;
+import com.example.outsourcing_11.domain.user.dto.UpdateUserAddressRequestDto;
+import com.example.outsourcing_11.domain.user.dto.UpdateUserPasswordRequestDto;
+import com.example.outsourcing_11.domain.user.dto.UpdateUserRequestDto;
 import com.example.outsourcing_11.domain.user.dto.UserResponseDto;
 import com.example.outsourcing_11.domain.user.entity.User;
 import com.example.outsourcing_11.domain.user.repository.UserRepository;
@@ -61,31 +65,12 @@ public class UserService {
 		return new UserResponseDto(findUser);
 	}
 
-	// public UserResponseDto updateUser(HttpServletRequest request) {
-	// 	// 쿠키 확인
-	// 	boolean valid = Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
-	// 		.anyMatch(cookie -> "delete_auth".equals(cookie.getName()) && "true".equals(cookie.getValue()));
-	//
-	// 	if (!valid) {
-	// 		throw new UnauthorizedException("수정 및 삭제 인증 쿠키가 없습니다.");
-	// 	}
-	// 	String token = request.getHeader("Authorization");
-	// 	Long userId = jwtUtil.extractUserId(token);
-	// 	User user = userRepository.findByIdOrElseThrow(userId);
-	// 	return new UserResponseDto();
-	// }
-
 	//수정전용 비밀번호 확인인증 쿠키
 	public void issueDeleteAuthCookie(PasswordRequestDto passwordDto,
-		HttpServletRequest request,
+		CustomUserDetails userDetails,
 		HttpServletResponse response) {
-		//  헤더에서 토큰 추출
-		String token = request.getHeader("Authorization");
-		if (!jwtUtil.validateToken(token)) {
-			throw new UnauthorizedException("유효하지 않은 토큰입니다."); // 401 에러
-		}
 		// 토큰에서 userId 추출
-		Long userId = jwtUtil.extractUserId(token);
+		Long userId = userDetails.getUserId();
 		User user = userRepository.findByIdOrElseThrow(userId);
 
 		// 비밀번호 확인
@@ -101,9 +86,83 @@ public class UserService {
 		response.addCookie(cookie);
 	}
 
+	// 이름 이메일(로그인아이디) 전화번호 수정
+	@Transactional
+	public void updateUser(UpdateUserRequestDto requestdto, CustomUserDetails userDetails, HttpServletRequest request) {
+		// 쿠키 인증
+		boolean valid = Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
+			.anyMatch(cookie -> "delete_auth".equals(cookie.getName()) && "true".equals(cookie.getValue()));
+
+		if (!valid) {
+			throw new UnauthorizedException("수정 및 삭제 인증 쿠키가 없습니다.");
+		}
+
+		// 유저 조회
+		Long userId = userDetails.getUserId();
+		User user = userRepository.findByIdOrElseThrow(userId);
+
+		// 변경 사항만 업데이트
+		if (!requestdto.getUserName().equals(user.getName())) {
+			user.updateName(requestdto.getUserName());
+		}
+
+		if (!requestdto.getEmail().equals(user.getEmail())) {
+			user.updateEmail(requestdto.getEmail());
+		}
+
+		if (!requestdto.getPhone().equals(user.getPhone())) {
+			user.updatePhone(requestdto.getPhone());
+		}
+	}
+
+	//주소수정
+	@Transactional
+	public void updateUserAddress(UpdateUserAddressRequestDto requestDto, CustomUserDetails userDetails,
+		HttpServletRequest request) {
+		// 쿠키 인증
+		boolean valid = Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
+			.anyMatch(cookie -> "delete_auth".equals(cookie.getName()) && "true".equals(cookie.getValue()));
+
+		if (!valid) {
+			throw new UnauthorizedException("수정 및 삭제 인증 쿠키가 없습니다.");
+		}
+
+		// 유저 조회
+		Long userId = userDetails.getUserId();
+		User user = userRepository.findByIdOrElseThrow(userId);
+
+		user.updateAddress(requestDto.getNewAddress());
+	}
+
+	//비번수정
+	@Transactional
+	public void updateUserPassword(UpdateUserPasswordRequestDto requestDto, CustomUserDetails userDetails,
+		HttpServletRequest request) {
+		// 쿠키 인증
+		boolean valid = Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
+			.anyMatch(cookie -> "delete_auth".equals(cookie.getName()) && "true".equals(cookie.getValue()));
+
+		if (!valid) {
+			throw new UnauthorizedException("수정 및 삭제 인증 쿠키가 없습니다.");
+		}
+
+		// 유저 조회
+		Long userId = userDetails.getUserId();
+		User user = userRepository.findByIdOrElseThrow(userId);
+
+		if (!passwordEncoder.matches(requestDto.getOldPassword(), user.getPassword())) { // 비번체크
+			throw new InvalidLoginException("비밀번호가 일치하지 않습니다"); // 401 반환
+		}
+
+		if (passwordEncoder.matches(requestDto.getNewPassword(), user.getPassword())) {
+			throw new InvalidUserInputException("현재 비밀번호와 동일합니다"); // 400 반환
+		}
+		user.updatePassword(passwordEncoder.encode(requestDto.getNewPassword()));
+	}
+
 	//쿠키를 통한 소프트 삭제
 	@Transactional
-	public DeleteUserResponseDto softDeleteUser(HttpServletRequest request) {
+	public DeleteAndUpdateUserResponseDto softDeleteUser(CustomUserDetails userDetails, HttpServletRequest request) {
 		// 쿠키 확인
 		boolean valid = Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
 			.anyMatch(cookie -> "delete_auth".equals(cookie.getName()) && "true".equals(cookie.getValue()));
@@ -113,11 +172,10 @@ public class UserService {
 		}
 
 		// 유저 삭제
-		String token = request.getHeader("Authorization");
-		Long userId = jwtUtil.extractUserId(token);
+		Long userId = userDetails.getUserId();
 		User user = userRepository.findByIdOrElseThrow(userId);
 		user.softDelete();
 
-		return new DeleteUserResponseDto("회원 탈퇴 완료", user.getDeletedAt());
+		return new DeleteAndUpdateUserResponseDto("회원 탈퇴 완료", user.getDeletedAt());
 	}
 }
