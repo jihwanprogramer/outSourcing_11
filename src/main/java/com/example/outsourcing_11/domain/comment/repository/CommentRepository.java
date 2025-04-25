04679a463c20c51802b932e7e1219c4b911cf86b
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2025-04-25 17:29:06 +0900

    fix :

--- src/main/java/com/example/outsourcing_11/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/outsourcing_11/domain/comment/repository/CommentRepository.java
@@ -27,6 +27,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
         return findByIdAndDeletedAtIsNull(commentId).orElseThrow(() -> new RuntimeException("Temp Error"));
     }
 
-    @Query("SELECT COUNT(c) FROM Comment c WHERE c.store.id = :storeId AND c.deletedAt IS NULL")
-    long countByStoreId(@Param("storeId") Long storeId);
+    @Query("SELECT COUNT(c) FROM Comment c JOIN c.order o JOIN o.items oi WHERE oi.menu.id = :menuId AND c.isDeleted = false")
+    long countByMenuId(@Param("menuId") Long menuId);
 }
