7043466ce3c5fc098860715689f61d98f9a943e8
Fragon02 <chj20202@naver.com>
2025-04-25 09:57:07 +0900

    feat: OwnerComment and crud       add Class Contructor fix : testCode

---
+++ src/main/java/com/example/outsourcing_11/domain/comment/dto/Owner/OwnerResponseCommentDto.java
@@ -0,0 +1,12 @@
+package com.example.outsourcing_11.domain.comment.dto.Owner;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class OwnerResponseCommentDto {
+
+	private final String content;
+
+}
