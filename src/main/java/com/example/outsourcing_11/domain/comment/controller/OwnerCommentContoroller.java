7770a24f9d85e5c6c37dc5a6295bafb9b049d81f
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2025-04-25 17:52:40 +0900

    fix :

--- src/main/java/com/example/outsourcing_11/domain/comment/controller/OwnerCommentContoroller.java
+++ src/main/java/com/example/outsourcing_11/domain/comment/controller/OwnerCommentContoroller.java
@@ -1,45 +1,38 @@
 package com.example.outsourcing_11.domain.comment.controller;
 
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.example.outsourcing_11.domain.comment.dto.Owner.OwnerRequestCommentDto;
 import com.example.outsourcing_11.domain.comment.dto.Owner.OwnerResponseCommentDto;
 import com.example.outsourcing_11.domain.comment.service.OwnerServiceimple;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("stores/{storeId}/")
 @RestController
 @RequiredArgsConstructor
 public class OwnerCommentContoroller {
 
-	private final OwnerServiceimple ownerServiceimple;
+    private final OwnerServiceimple ownerServiceimple;
 
-	@PostMapping("orders/{orderId}/comments{commentId}/repley")
-	private ResponseEntity<OwnerResponseCommentDto> createOwnerComment(@RequestBody OwnerRequestCommentDto dto) {
+    @PostMapping("orders/{orderId}/comments{commentId}/repley")
+    private ResponseEntity<OwnerResponseCommentDto> createOwnerComment(@RequestBody OwnerRequestCommentDto dto) {
 
-		return new ResponseEntity<>(ownerServiceimple.creatOwerComment(dto), HttpStatus.OK);
-	}
+        return new ResponseEntity<>(ownerServiceimple.creatOwerComment(dto), HttpStatus.OK);
+    }
 
-	@PutMapping("orders/{orderId}/comments{commentId}/repley/{id}")
-	private ResponseEntity<OwnerResponseCommentDto> updateOwnerComment(
-		@PathVariable Long id,
-		@RequestBody OwnerRequestCommentDto dto) {
+    @PutMapping("orders/{orderId}/comments{commentId}/repley/{id}")
+    private ResponseEntity<OwnerResponseCommentDto> updateOwnerComment(
+        @PathVariable Long id,
+        @RequestBody OwnerRequestCommentDto dto) {
 
-		return new ResponseEntity<>(ownerServiceimple.updateOwerComment(id, dto), HttpStatus.OK);
-	}
+        return new ResponseEntity<>(ownerServiceimple.updateOwerComment(id, dto), HttpStatus.OK);
+    }
 
-	@DeleteMapping("stores/{storeId}/orders/{orderId}/comments{commentId}/repley/{id}")
-	private ResponseEntity<Void> deleteOwnerComment(@PathVariable Long id) {
+    @DeleteMapping("orders/{orderId}/comments{commentId}/repley/{id}")
+    private ResponseEntity<Void> deleteOwnerComment(@PathVariable Long id) {
 
-		ownerServiceimple.deleteOwerComment(id);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+        ownerServiceimple.deleteOwerComment(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/outsourcing_11/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/outsourcing_11/domain/comment/repository/CommentRepository.java
@@ -1,37 +1,41 @@
 package com.example.outsourcing_11.domain.comment.repository;
 
-import java.util.Optional;
-
+import com.example.outsourcing_11.domain.comment.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import com.example.outsourcing_11.domain.comment.entity.Comment;
+
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-	//commentId에 해당하는 comment 조회
-	Optional<Comment> findByIdAndDeletedAtIsNull(Long comentId);
+    //commentId에 해당하는 comment 조회
+    Optional<Comment> findByIdAndDeletedAtIsNull(Long comentId);
 
-	//별점 범위에 따른 comment 조회
+    //별점 범위에 따른 comment 조회
 
-	/**
-	 2025.04.25
-	 * 김형진
-	 * 별점 범위 이면서 soft Delete가 적용된거 외에 것을 조회
-	 * @param min ~0
-	 * @param max ~5
-	 * @return comments
-	 */
-	Page<Comment> findByRatingBetweenAndDeletedAtIsNull(int min, int max, Pageable pageable);
+    /**
+     * 2025.04.25
+     * 김형진
+     * 별점 범위 이면서 soft Delete가 적용된거 외에 것을 조회
+     *
+     * @param min ~0
+     * @param max ~5
+     * @return comments
+     */
+    Page<Comment> findByRatingBetweenAndDeletedAtIsNull(int min, int max, Pageable pageable);
 
-	@Query("SELECT c FROM Comment c JOIN Fetch  Order o WHERE o.id = :orderId AND c.isDeleted = false ")
-	Optional<Comment> findWithRelationsByOrderId(@Param("orderId") Long orderId);
+    @Query("SELECT c FROM Comment c JOIN Fetch  Order o WHERE o.id = :orderId AND c.isDeleted = false ")
+    Optional<Comment> findWithRelationsByOrderId(@Param("orderId") Long orderId);
 
-	default Comment findByOrThrowElse(Long commentId) {
-		return findByIdAndDeletedAtIsNull(commentId).orElseThrow(() -> new RuntimeException("Temp Error"));
-	}
+    @Query("SELECT COUNT(c) FROM Comment c WHERE c.store.id = :storeId AND c.deletedAt IS NULL")
+    long countByStoreId(@Param("storeId") Long storeId);
+
+    default Comment findByOrThrowElse(Long commentId) {
+        return findByIdAndDeletedAtIsNull(commentId).orElseThrow(() -> new RuntimeException("Temp Error"));
+    }
 }
