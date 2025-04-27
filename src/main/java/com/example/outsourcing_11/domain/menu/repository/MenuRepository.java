7721c8ba8360e4abf0b50a05cded8b7e8d48b69b
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2025-04-27 13:35:27 +0900

    fix: 메뉴 메소드 추가

--- src/main/java/com/example/outsourcing_11/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/outsourcing_11/domain/menu/repository/MenuRepository.java
@@ -1,16 +1,13 @@
 package com.example.outsourcing_11.domain.menu.repository;
 
-import java.util.List;
-
+import com.example.outsourcing_11.domain.menu.entity.Menu;
+import com.example.outsourcing_11.domain.menu.enums.Category;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import com.example.outsourcing_11.domain.menu.entity.Menu;
-import com.example.outsourcing_11.domain.menu.enums.Category;
-import com.example.outsourcing_11.domain.store.entity.Store;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
