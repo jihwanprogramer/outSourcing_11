9095ba36834e89c4f8be545232c2fdd925fe11b6
escomputer <nujest307@naver.com>
2025-04-28 07:53:27 +0900

    fix : dto반환 -> 엔티티 반환

--- src/main/java/com/example/outsourcing_11/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/outsourcing_11/domain/store/repository/StoreRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.outsourcing_11.domain.store.dto.StoreResponseDto;
 import com.example.outsourcing_11.domain.store.entity.Store;
 import com.example.outsourcing_11.domain.store.entity.StoreCategory;
 import com.example.outsourcing_11.domain.store.entity.StoreStatus;
@@ -26,7 +25,7 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
 	Optional<Store> findByIdAndDeletedFalse(Long storeId);
 
-	List<StoreResponseDto> findAllByOwnerAndDeletedFalse(User user);
+	List<Store> findAllByOwnerAndDeletedFalse(User user);
 
 	Optional<Store> findByIdAndOwnerAndDeletedFalse(Long storeId, User user);
 }
