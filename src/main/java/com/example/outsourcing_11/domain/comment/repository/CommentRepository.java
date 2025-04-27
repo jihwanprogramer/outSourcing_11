deca30f385c352841bbcd4197debd40eea4682c4
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2025-04-27 13:35:20 +0900

    fix: 메뉴 메소드 추가

--- src/main/java/com/example/outsourcing_11/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/outsourcing_11/domain/comment/repository/CommentRepository.java
@@ -32,7 +32,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT c FROM Comment c JOIN Fetch  Order o WHERE o.id = :orderId AND c.isDeleted = false ")
     Optional<Comment> findWithRelationsByOrderId(@Param("orderId") Long orderId);
 
-    @Query("SELECT COUNT(c) FROM Comment c JOIN c.order o JOIN o.items oi WHERE oi.menu.id = :menuId AND c.isDeleted = false")
+    @Query("SELECT COUNT(c) FROM Comment c JOIN c.order o JOIN o.items oi WHERE oi.menu.id = :menuId AND c.parent IS NULL AND c.isDeleted = false")
     long countByMenuId(@Param("menuId") Long menuId);
 
     default Comment findByOrThrowElse(Long commentId) {
