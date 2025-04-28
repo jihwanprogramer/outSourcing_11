577a7157f412972932fa8131e634b025543ae2ac
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2025-04-28 13:27:06 +0900

    fix:

--- src/main/java/com/example/outsourcing_11/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/outsourcing_11/domain/menu/repository/MenuRepository.java
@@ -1,7 +1,10 @@
 package com.example.outsourcing_11.domain.menu.repository;
 
+import com.example.outsourcing_11.common.exception.menu.MenuCustomException;
+import com.example.outsourcing_11.common.exception.menu.MenuErrorCode;
 import com.example.outsourcing_11.domain.menu.entity.Menu;
 import com.example.outsourcing_11.domain.menu.enums.Category;
+import com.example.outsourcing_11.domain.store.entity.Store;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,27 +12,29 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     default Menu finByIdOrElseThrow(Long menuId) {
-        return findById(menuId).orElseThrow(() -> new MenuNotFoundException("존재하지 않는 메뉴입니다"));
+        return findById(menuId).orElseThrow(() -> new MenuCustomException(MenuErrorCode.MENU_NOT_FOUND));
     }
 
-	@Query("""
-		    SELECT m FROM Menu m
-		    WHERE m.store.id = :storeId
-		    AND (:categoryCursor IS NULL
-		           OR m.category > :categoryCursor
-		           OR (m.category = :categoryCursor AND m.id > :lastId)
-		          )
-		    AND m.deletedAt IS NULL
-		    ORDER BY m.category ASC, m.id ASC
-		""")
-	Slice<Menu> findByCursorMenuBySize(@Param("storeId") Long storeId,
-		@Param("categoryCursor") Category categoryCursor,
-		@Param("lastId") Long lastId,
-		Pageable pageable);
+    @Query("""
+            SELECT m FROM Menu m
+            WHERE m.store.id = :storeId
+            AND (:categoryCursor IS NULL
+                   OR m.category > :categoryCursor
+                   OR (m.category = :categoryCursor AND m.id > :lastId)
+                  )
+            AND m.deletedAt IS NULL
+            ORDER BY m.category ASC, m.id ASC
+        """)
+    Slice<Menu> findByCursorMenuBySize(@Param("storeId") Long storeId,
+                                       @Param("categoryCursor") Category categoryCursor,
+                                       @Param("lastId") Long lastId,
+                                       Pageable pageable);
 
-	List<Menu> findByStoreAndDeletedAtIsNull(Store store);
+    List<Menu> findByStoreAndDeletedAtIsNull(Store store);
 }
 
